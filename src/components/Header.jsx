c6c2e7d38105f35b0f387a5231152e282e222365
upendhar Nemmani <upendharnemmani@gmail.com>
2025-03-29 00:13:21 +0530

    fix(Login) : fix user logout in the Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,10 @@ import React from "react";
 import assets from "../assets/assets";
 
 function Header() {
+  const handleLogOut = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
   return (
     <div className="fixed flex min-h-8 w-full items-center justify-between bg-pink-50 px-5">
       <img
@@ -10,7 +14,10 @@ function Header() {
         alt="EmployWise"
         className="max-h-[50px]"
       />
-      <button className="flex h-full items-center justify-center gap-3 rounded-md bg-fuchsia-400 px-3 py-1 font-bold text-white hover:bg-fuchsia-500">
+      <button
+        onClick={handleLogOut}
+        className="flex h-full items-center justify-center gap-3 rounded-md bg-fuchsia-400 px-3 py-1 font-bold text-white hover:bg-fuchsia-500"
+      >
         LogOut
       </button>
     </div>
